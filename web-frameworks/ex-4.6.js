e8b280ec04c0c30a4b7772347184b888e1ce7299
mohamed <khaldimohamed82@gmail.com>
2024-04-10 00:28:36 +0000

    Notes from my learning journey

--- web-frameworks/ex-4.6.js
+++ web-frameworks/ex-4.6.js
@@ -3,9 +3,16 @@ import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hi there!"));
-app.get("/congrats", (c) => c.text("Congrats, {heroOfTheDay}!"));
-app.on("cat", "/secrets", (c) => c.text("Meow!"));
-app.on("whats", "/up", (c) => c.text("A movie!"));
 
+app.get("/congrats", (c) => c.text(`Congrats, ${c.req.query("heroOfTheDay")}!`));
+
+app.get("/items/:id", (c) => {
+    const idItems = c.req.param("id")
+    return c.text(`Retrieving item ${idItems}.`)
+    
+})
+
+app.post("/items", (c) => c.text("Adding an item."))
 
 export default app;
+
