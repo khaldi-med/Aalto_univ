9003471cf5b27fb53caee18f6574861d9583d719
mohamed <khaldimohamed82@gmail.com>
2024-05-05 01:16:39 +0100

    My learning journey practice

---
+++ view-temp/ex-8.2-04/app.js
@@ -0,0 +1,16 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const app = new Hono();
+
+const data = {
+  vegetables: ["Carrot", "Kale", "Broccoli", "Zombie"],
+};
+
+app.get("/", (c) => {
+  return c.html(eta.render("index.eta", data));
+});
+
+export default app;
