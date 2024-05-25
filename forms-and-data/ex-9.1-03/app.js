52dbea7368ce7e2bc46b4f8bf5d97bc1860cc6eb
mohamed <khaldimohamed82@gmail.com>
2024-05-25 16:05:36 +0100

    My learning journey practice

---
+++ forms-and-data/ex-9.1-03/app.js
@@ -0,0 +1,24 @@
+import {
+  Eta
+} from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import {
+  Hono
+} from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const eta = new Eta({
+  views: `${Deno.cwd()}/templates/`
+});
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.html(eta.render("index.eta"));
+});
+app.post("/", async (c) => {
+  const body = await c.req.parseBody();
+  console.log(`${body.name} lives at ${body.address}.`);
+  //return c.text("OK");
+});
+
+Deno.serve(app.fetch);
+
